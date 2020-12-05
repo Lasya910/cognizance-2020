f911a33137bc91ae3322cbe7dafa251fdfe1022c
Lasya910 <csea.15190@ch.students.amrita.edu>
2020-12-05 21:06:04 +0530

    first commit

---
+++ fibonnaci.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main (void);
+{
+    int i,n1,n2,n3,num;
+        cout<<"enter the number";
+        cin>>num;
+        cout<<"fibonnaci series is";
+        for(i=2;i<=num;i++)
+        {
+            n3=n1+n2; 
+        cout<<"\n"<<n3;
+        n1=n2;
+        n2=n3;
+        }
+        
+
+           
+}
